c4b7dd991573bbc70c8048124bc2fcaaa1e0d60e
Anam Asif <anam.asif285@gmail.com>
2021-09-30 14:33:35 +0100

    price

--- src/App.js
+++ src/App.js
@@ -2,155 +2,121 @@
 import './App.css';
 // import CatApi from './CatApi';
 import {useState, useEffect} from 'react';
+const faker = require('faker')
 
 function App() {
   const [sibr, setSibr]= useState([])
   const [beng, setBeng]= useState([])
 
-
-
-
-  
-  useEffect(()=>{handleFetchSibr()
-
+  useEffect(()=>{
+    handleFetchSibr()
   },[])
-  useEffect(()=>{handleFetchBeng()
 
+  useEffect(()=>{
+    handleFetchBeng()
   },[])
 
  
 
 
-  const handleFetchBeng =async()=>{
-    let newdata=[]
+  const handleFetchBeng = async () => {
+    let newData = []
 
-    let response =  await fetch("https://api.thecatapi.com/v1/images/search?breed_ids=beng&limit=4"
+    let response =  await fetch("https://api.thecatapi.com/v1/images/search?breed_ids=beng&limit=4"); 
+    newData = await response.json();
+    
+    for (let i = 0; i < newData.length; i++) {
+      const item = newData[i];
+      item.price = faker.commerce.price()
+    }
 
-    ); 
-    newdata = await response.json();
-    setBeng(newdata)
-  
-  
-  }
-  const handleFetchSibr =async()=>{
-    let newdata=[]
-
-    let response =  await fetch("https://api.thecatapi.com/v1/images/search?breed_ids=sibe&limit=4"
-
-    ); 
-    newdata = await response.json();
-    setSibr(newdata)
-  
+    setBeng(newData)
   
   }
 
+  const handleFetchSibr = async () => {
+    let newData = []
 
+    let response =  await fetch("https://api.thecatapi.com/v1/images/search?breed_ids=sibe&limit=4"); 
+    newData = await response.json();
+    for (let i = 0; i < newData.length; i++) {
+      const item = newData[i];
+      item.price = faker.commerce.price()
+    }
+
+
+
+    setSibr(newData)
   
-
-
-
-
-
-
-
-
-
-
-
+  }
 
   return (
     <div className="App">
-      <h1>Pet Shop</h1>
-    
+      {beng.length > 0 && sibr.length > 0 ? 
+        <>
+          <h1>Pet Shop</h1>
+          <div className="wrapperBeng">
+            <Catdesc data={sibr}/>
+            <CatInfo data={sibr}/>
+            <button onClick={handleFetchSibr}>More Siberian</button>
+          </div>
+          <div className="wrapperBeng">
+            <Catdesc data={beng}/>
+            <CatInfo data={beng}/>
+            <button onClick={handleFetchBeng}>More BengalCats</button>
+            </div>
+                  </>
+      :
+        <h3>Loading...</h3>
+      }
       
-      <Catdesc data={sibr}/>
-      
-        <CatInfo data={sibr}/>
-        <button onClick={handleFetchSibr}>More Siberian</button>
-     
-
-      
-      <Catdesc data={beng}/>
-      
-        <CatInfo data={beng}/>
-        <button onClick={handleFetchBeng}>More BengalCats</button>
-      
-       
-       
-
-      
-      
-     
-     
-
-
-     
-     
-     
     </div>
   );
 }
 
-const CatInfo =(props)=>{
+const CatInfo = (props) => {
   return (
      <div className="wrapper">
-      
       <CatImg data={props.data} />
       
-      
     </div>
   )
 }
-const Detail =(props)=>{
-  return(
+const Detail = (props) => {
+  return (
     <>
-
-    <div className="wrapperCat">
-      <img src={props.imgSrc} alt="cant access image"/>
-      
-      {props.breed}
-    
-    </div>
+      <div className="wrapperCat">
+        <img src={props.imgSrc} alt="cant access image"/>
+        {/* {props.breed} */}
+        <h4>Price: £{props.price}</h4>
+      </div>
     </>
-
   )
-
 }
-const Catdesc=(props)=>{
-  let disc=props.data[0]
-  console.log(disc)
-  return(
+
+const Catdesc = (props) => {
+  console.log(props.data)
+  return (
       <p>
-      {disc.breeds[0].description}
-      </p>)
+        {props.data[0].breeds[0].description}
+      </p>
+  )
 }
-
-
-
-
 
 
 const CatImg =(props) => {
   return(
-
     props.data.map((item,index)=>{
       return (
         <Detail 
-
           imgSrc={item.url}
           key = {index}
           breed= {item.breeds[0].name}
-          
-          
-          />
+          price= {item.price}
 
-
+        />
       )
-
     })
-
-
-
   )
 }
 
